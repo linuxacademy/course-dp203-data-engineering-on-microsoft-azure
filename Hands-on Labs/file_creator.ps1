5ec27ba40da9a8a84713b5f2f66a6daa00df2631
Landon Fowler <landon.fowler@linuxacademy.com>
2021-04-20 10:08:36 -0400

    Update file creator

--- Hands-on Labs/file_creator.ps1
+++ Hands-on Labs/file_creator.ps1
@@ -1,3 +1,11 @@
+##########################################################################
+# Source: A Cloud Guru "DP-203 Data Engineering on Microsoft Azure" Course
+# Author: Landon Fowler
+# Purpose: Script to simulate streaming data into an Azure Data Lake.
+#   It will continuously create JSON files in two different containers.
+# Date Updated: 10/05/2020
+##########################################################################
+
 #Parameters
 param(
         [string]$storageAccountName,
@@ -12,11 +20,10 @@ $ukCounter =1
 # Set storage context
 $ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
 
-
-# Infinite Loop to reproduce continuous stream
-while (1 -lt 2) {
+# Infinite Loop to simulate continuous stream
+while (1 -lt 2) {  
     
-    
+    # Create files for the "usa" container
     if ($containerNum -eq 0) {
         $weatherObject = [PSCustomObject]@{
             country     = 'United States'
@@ -24,15 +31,21 @@ while (1 -lt 2) {
             low = Get-Random -Minimum 0 -Maximum 50
         }
 
+        # Name, create, and upload the source file
         $sourceFile = "weather_usaUser$usaCounter.json"
         $weatherObject | ConvertTo-Json > $sourceFile
         New-AzDataLakeGen2Item -Context $ctx -FileSystem "usa" -Path $sourceFile -Source $sourceFile -Force
 
+        # Remove the source file after we're finished with it
         Remove-Item $sourceFile
 
+        # Swap to the UK for the next round
         $containerNum = 1
+
+        # Increment the user number to ensure unique file names
         $usaCounter++
     }
+    # Create files for the "uk1" container
     elseif ($containerNum -eq 1) {
         $weatherObject = [PSCustomObject]@{
             country     = 'United Kingdom'
@@ -40,17 +53,22 @@ while (1 -lt 2) {
             low = Get-Random -Minimum 0 -Maximum 40
         }
 
+        # Name, create, and upload the source file
         $sourceFile = "weather_ukUser$ukCounter.json"
         $weatherObject | ConvertTo-Json > $sourceFile
         New-AzDataLakeGen2Item -Context $ctx -FileSystem "uk1" -Path $sourceFile -Source $sourceFile -Force
 
+        # Remove the source file after we're finished with it
         Remove-Item $sourceFile
 
+        # Swap to the UK for the next round
         $containerNum = 0
+
+        # Increment the user number to ensure unique file names
         $ukCounter++
     }
 
-
-    Start-Sleep -Seconds 2
+    # Sleep between 1-5 seconds so that it's more like random users uploading and less like a machine fire hose
+    Start-Sleep -Seconds (Get-Random -Minimum 1 -Maximum 5)
 
 }
